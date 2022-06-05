94b3e5dc81b9d158557ff40445000ae8b48b40b3
Prachurja Kanti Barman Praggo <86295748+Prachurja-Kanti@users.noreply.github.com>
2022-06-05 22:36:31 +0600

    Add files via upload

---
+++ Expense Tracker/expenseTrakerApp.py
@@ -0,0 +1,21 @@
+from datetime import date
+import csv
+
+dt = date.today()
+dt = dt.strftime("%d/%m/%y")
+filename = "test.csv"
+exp = []
+stopped = False
+
+with open(filename, 'a', newline="") as file:
+    csvwriter = csv.writer(file)
+    while not stopped:
+        xp = int(input("What is the expense(type 0 to stop): "))
+        if xp==0:
+            stopped = True
+        else:
+            csvwriter.writerow([dt,xp])
+            exp.append(xp)
+file.close()
+print("Your expenses todays are: ", exp)
+print("Your total expense is: ", sum(exp))
